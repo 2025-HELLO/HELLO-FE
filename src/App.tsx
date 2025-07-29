fa256556eb11fc86a127487bb7a18199d01b289e
임지수 <dlawltn0708@naver.com>
2025-07-29 15:52:35 +0900

    init: tanstack query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { queryClient } from "./common/utils/queryClient";
+
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
 }
 
 export default App;
