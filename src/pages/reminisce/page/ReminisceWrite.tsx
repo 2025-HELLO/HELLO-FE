fa6f4b3b2a290962ca71a2faad19b8ad9b4348a0
임지수 <dlawltn0708@naver.com>
2025-08-12 22:59:56 +0900

    feat: 회상노트 작성 페이지 구현

--- src/pages/reminisce/page/ReminisceWrite.tsx
+++ src/pages/reminisce/page/ReminisceWrite.tsx
@@ -1,7 +1,60 @@
-const ReminisceWrite = () => (
-  <section>
-    <h2>회상노트 작성</h2>
-  </section>
-);
+import React, { useState } from 'react';
+
+import * as s from './ReminisceWrite.css';
+
+import { IcMike } from '@/assets/svgs';
+import Button from '@/common/components/button/Button';
+
+const ReminisceWrite = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className={s.container}>
+      <h1 className={s.title}>기록을 입력해주세요</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={s.card}>
+          <input
+            type="text"
+            placeholder="제목을 입력해주세요"
+            className={s.inputTitle}
+            value={title}
+            onChange={handleTitleChange}
+          />
+          <textarea
+            placeholder="내용을 입력해주세요"
+            className={s.inputContent}
+            rows={6}
+            value={content}
+            onChange={handleContentChange}
+          />
+          <div className={s.mikeButton}>
+            <IcMike className={s.mikeIcon} />
+          </div>
+        </div>
+        <div className={s.buttonContainer}>
+          <Button
+            variant="secondary"
+            label="저장하기"
+            type="submit"
+            disabled={title === '' || content === ''}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
 
 export default ReminisceWrite;
