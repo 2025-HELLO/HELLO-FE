8f8db6180c48882e504427cd008a6cc54366b75a
임지수 <dlawltn0708@naver.com>
2025-09-07 14:31:51 +0900

    feat: STT 구현

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,4 +1,3 @@
-export { default as Group } from './Group';
 export { default as IcBack } from './IcBack';
 export { default as IcClock } from './IcClock';
 export { default as IcDownarrow } from './IcDownarrow';
@@ -15,6 +14,7 @@ export { default as IcMike } from './IcMike';
 export { default as IcPlus } from './IcPlus';
 export { default as IcProfile } from './IcProfile';
 export { default as IcSend } from './IcSend';
+export { default as IcStop } from './IcStop';
 export { default as IcTrash } from './IcTrash';
 export { default as IcUparrow } from './IcUparrow';
 export { default as IcUser } from './IcUser';
